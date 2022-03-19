68e50febdb77a48db5db453ed179bcddb37159bf
Chris Agold <christophersagold@gmail.com>
2022-03-19 12:26:35 -0500

    updated .gitignore

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,8 +2,24 @@
 // import logo from '../assets/images/main.svg';
 
 const Landing = () => {
+    const username =
+    const [username, setUsername] = useState('');
     return (
-        <h4>LANDING PAGE</h4>
+        <>
+            <h4>LANDING PAGE</h4>
+            <input
+            onChange={event=>setUsername(event.target.value)}
+            />
+            <input/>
+            <button
+                /*
+                onClick, call the endpoint that validates the user and password
+                //onClick={()=>login(username,password)}
+                //login is an async func that submits user,pass tobackend for validation
+                * */
+            onClick={}
+            />
+        </>
     );
 }
 
