8acf2ca11b677adfe763409fe6a87154ce12ff4d
Chris Agold <christophersagold@gmail.com>
2022-03-26 18:52:20 -0500

    added styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
 import LandingPage from './pages/landing';
 
 function App() {
   return (
       <div>
-        <h1>sleeperPC</h1>
         <LandingPage/>
       </div>
   );
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,10 +2,9 @@
 // import logo from '../assets/images/main.svg';
 import styled from 'styled-components';
 
-
 const Landing = () => {
     return (
-       <main>
+       <Wrapper>
            {/*NAV BAR*/}
            <nav>
                {/*ADD NAV LOGO*/}
@@ -14,15 +13,36 @@ const Landing = () => {
                        <h1>
                            Sleeper  <span>PC</span> APP
                        </h1>
-                       <p></p>
+                       <p>Text</p>
                        <button className="">Login/Register</button>
                    </div>
                </div>
+               {/*Add image later*/}
                <img/>
            </nav>
-       </main>
+       </Wrapper>
     );
 }
 
+// STYLED COMPONENTS
+const Wrapper = styled.main `
+nav {
+  width: var(--fluid-width);
+  max-width: var(--max-width);
+  margin: 0 auto;
+  height: var(--nav-height);
+  display: flex;
+  align-items: center;
+}
+  h1 {
+    font-weight: 700;
+    span {
+      color: darkgray;
+    }
+  }
+  p {
+    color: grey;
+  }
+`
 
 export default Landing;
