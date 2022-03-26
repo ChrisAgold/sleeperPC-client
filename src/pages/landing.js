41c60a6afe32e97641fcee167dcbb194fb5ecb3c
Chris Agold <christophersagold@gmail.com>
2022-03-26 17:43:47 -0500

    added landing page content

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,24 +2,23 @@
 // import logo from '../assets/images/main.svg';
 
 const Landing = () => {
-    const username =
-    const [username, setUsername] = useState('');
     return (
-        <>
-            <h4>LANDING PAGE</h4>
-            <input
-            onChange={event=>setUsername(event.target.value)}
-            />
-            <input/>
-            <button
-                /*
-                onClick, call the endpoint that validates the user and password
-                //onClick={()=>login(username,password)}
-                //login is an async func that submits user,pass tobackend for validation
-                * */
-            onClick={}
-            />
-        </>
+       <main>
+           {/*NAV BAR*/}
+           <nav>
+               {/*ADD NAV LOGO*/}
+               <div className="container">
+                   <div className="info">
+                       <h1>
+                           Sleeper  <span>PC</span> APP
+                       </h1>
+                       <p></p>
+                       <button className="">Login/Register</button>
+                   </div>
+               </div>
+               <img/>
+           </nav>
+       </main>
     );
 }
 
